bde34e7ec2b07b7ddba76339869158161bd3a8ee
Filip V <56982870+theowlf@users.noreply.github.com>
2021-01-02 18:17:24 +0100

    Added an alert for a successfull booking

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,15 @@ const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking') {
+    res.locals.alert =
+      'Your booking was successful. Please check your email for a receipt';
+  }
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data form collection
   const tours = await Tour.find();
--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { displayMap } from './mapbox';
 import { logIn, logOut } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM Elements
 const mapBox = document.getElementById('map');
@@ -67,3 +68,7 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+
+if (alertMessage) showAlert('success', alertMessage, 10);

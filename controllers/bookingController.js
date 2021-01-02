bd7f57398a6e494d04e0af5d5a49b14375ec8a2f
Filip V <56982870+theowlf@users.noreply.github.com>
2021-01-02 17:49:20 +0100

    fixed problem where successfull payment was not creating a booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,8 +53,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  const price = session.amount_total / 100;
+  await Booking.create({ tour: tour, user: user, price: price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
